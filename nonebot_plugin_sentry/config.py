041cb4dfad7967819b145290006dcc8e823731e8
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2024-05-06 09:25:13 +0000

    :arrow_up: upgrade dependencies

--- nonebot_plugin_sentry/config.py
+++ nonebot_plugin_sentry/config.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Optional
+from typing import Any, Optional
 
 from nonebot import get_driver
 from pydantic import Field, BaseModel
@@ -6,7 +6,7 @@ from sentry_sdk.integrations import Integration
 from nonebot.compat import PYDANTIC_V2, ConfigDict
 from sentry_sdk.integrations.loguru import LoguruIntegration
 
-from .compat import field_validator, model_validator
+from .compat import model_validator
 
 driver = get_driver()
 
@@ -14,7 +14,7 @@ driver = get_driver()
 class Config(BaseModel):
     sentry_dsn: Optional[str] = None
     sentry_environment: str = driver.env
-    sentry_integrations: List[Integration] = Field(
+    sentry_integrations: list[Integration] = Field(
         default_factory=lambda: [LoguruIntegration()]
     )
 
@@ -22,9 +22,7 @@ class Config(BaseModel):
     # sentry_default_integrations: bool = False
 
     if PYDANTIC_V2:
-        model_config: ConfigDict = ConfigDict(
-            extra="allow", arbitrary_types_allowed=True
-        )
+        model_config = ConfigDict(extra="allow", arbitrary_types_allowed=True)
     else:
 
         class Config:
@@ -33,14 +31,16 @@ class Config(BaseModel):
 
     @model_validator(mode="before")
     @classmethod
-    def filter_sentry_configs(cls, values: Dict[str, Any]):
+    def filter_sentry_configs(cls, values: dict[str, Any]):
         return {
             key: value for key, value in values.items() if key.startswith("sentry_")
         }
 
-    @field_validator("sentry_integrations", mode="after")
-    def validate_integrations(cls, v: List[Integration]):
-        ids = {i.identifier for i in v}
-        if LoguruIntegration.identifier not in ids:
-            v.append(LoguruIntegration())
-        return v
+    # [FIXED] https://github.com/getsentry/sentry-python/pull/2671
+    # LoguruIntegration is auto enabled by sentry_sdk now
+    # @field_validator("sentry_integrations", mode="after")
+    # def validate_integrations(cls, v: list[Integration]):
+    #     ids = {i.identifier for i in v}
+    #     if LoguruIntegration.identifier not in ids:
+    #         v.append(LoguruIntegration())
+    #     return v
